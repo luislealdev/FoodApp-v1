f65a95a753d006b73b4b94a49e112131e2abfa0a
Alfredo <21030761@itcelaya.edu.mx>
2023-06-13 16:38:33 -0600

    RecyclerView reder problem fixed (Home Fragment)

--- app/src/main/java/luisrrleal/com/foodapp_v1/fragments/Home_Fragment.java
+++ app/src/main/java/luisrrleal/com/foodapp_v1/fragments/Home_Fragment.java
@@ -17,11 +17,12 @@ import java.util.ArrayList;
 import luisrrleal.com.foodapp_v1.Domain.Popular_food;
 
 public class Home_Fragment extends Fragment{
-
     private RecyclerView recyclerView_food;
     ArrayList<Popular_food> cards = new ArrayList<>();
-    MainActivity activity = new MainActivity();
 
+    public Home_Fragment() {
+
+    }
     public static Home_Fragment newInstance() {
         Home_Fragment fragment = new Home_Fragment();
         Bundle args = new Bundle();
@@ -32,13 +33,7 @@ public class Home_Fragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    private void food_cards_Setup(){
         fill_cards_info();
-        LinearLayoutManager rv_layoutManager = new LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL,false);
-        recyclerView_food.setLayoutManager(rv_layoutManager);
-        recyclerView_food.setAdapter(new Popular_food_adapter(cards));
     }
 
     public void fill_cards_info(){
@@ -62,10 +57,11 @@ public class Home_Fragment extends Fragment{
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
-    /*
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         recyclerView_food = (RecyclerView) getView().findViewById(R.id.recyclerView_food_id);
-        food_cards_Setup();
-    }*/
+        LinearLayoutManager rv_layoutManager = new LinearLayoutManager(getView().getContext(), LinearLayoutManager.HORIZONTAL,false);
+        recyclerView_food.setLayoutManager(rv_layoutManager);
+        recyclerView_food.setAdapter(new Popular_food_adapter(cards));
+    }
 }
