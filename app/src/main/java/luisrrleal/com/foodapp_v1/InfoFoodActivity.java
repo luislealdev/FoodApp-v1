5bed380d1f9c392929322da319d2922078ddf097
Alfredo <21030761@itcelaya.edu.mx>
2023-06-19 23:10:18 -0600

    Demo of activiti_info layout that would be shown after a food card has been prressed (New activity created)

---
+++ app/src/main/java/luisrrleal/com/foodapp_v1/InfoFoodActivity.java
@@ -0,0 +1,14 @@
+package luisrrleal.com.foodapp_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_food);
+    }
+}
