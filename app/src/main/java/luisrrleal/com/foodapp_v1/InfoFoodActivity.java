7ff49ed40631ad41a8ade536ac1bfae857f0403a
Alfredo <21030761@itcelaya.edu.mx>
2023-06-25 12:42:20 -0600

    Fixed styles problems with bg color of activities and added click event to help return to Main Avtivity

--- app/src/main/java/luisrrleal/com/foodapp_v1/InfoFoodActivity.java
+++ app/src/main/java/luisrrleal/com/foodapp_v1/InfoFoodActivity.java
@@ -2,13 +2,25 @@ package luisrrleal.com.foodapp_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class InfoFoodActivity extends AppCompatActivity {
 
+    ImageView back_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_food);
+        back_button = (ImageView) findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), MainActivity.class);
+                v.getContext().startActivity(i);
+            }
+        });
     }
 }
