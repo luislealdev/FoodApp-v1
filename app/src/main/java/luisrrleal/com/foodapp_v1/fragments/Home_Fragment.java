7ea84b52c0a70090a6e652093460e8bb8bcff95e
Alfredo <21030761@itcelaya.edu.mx>
2023-07-09 14:01:25 -0600

    Method to search in recyclerView of food with EditText and hom fragment refactor temporaly

--- app/src/main/java/luisrrleal/com/foodapp_v1/fragments/Home_Fragment.java
+++ app/src/main/java/luisrrleal/com/foodapp_v1/fragments/Home_Fragment.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import luisrrleal.com.foodapp_v1.Adapter.Popular_food_adapter;
 import luisrrleal.com.foodapp_v1.Adapter.Popular_food_adapter2;
@@ -15,13 +20,20 @@ import luisrrleal.com.foodapp_v1.Adapter.Sections_adapter;
 import luisrrleal.com.foodapp_v1.Domain.Data_Provider;
 import luisrrleal.com.foodapp_v1.R;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Home_Fragment extends Fragment{
+    private EditText search_bar;
     private RecyclerView recyclerView_food;
     private RecyclerView recyclerView_sections;
 
     ArrayList<Data_Provider> sections = new ArrayList<>();
     ArrayList<Data_Provider> cards = new ArrayList<>();
+
+    //Esta arrayList lo ocupamos para guardar las cards filtradas por el buscador
+    List<Data_Provider> cardsAux = new ArrayList<Data_Provider>();
+    Popular_food_adapter2 vertical_cards = new Popular_food_adapter2(cards);
 
     public Home_Fragment() {
 
@@ -36,8 +48,33 @@ public class Home_Fragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //searchItems();
         fill_cards_info();
         fill_section_info();
+    }
+
+    public void set_Search_bar(){
+        search_bar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                String valueTyped = s.toString().toLowerCase();
+                cardsAux = cards.stream()
+                        .filter(cardItem -> cardItem.getCardTitle().toLowerCase().contains(valueTyped))
+                        .collect(Collectors.toList());
+                vertical_cards.updateCardsBySearch(cardsAux);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     public void fill_cards_info(){
@@ -76,15 +113,17 @@ public class Home_Fragment extends Fragment{
         recyclerView_sections.setAdapter(new Sections_adapter(sections));
 
         //Get the recycelrView of popular food and render it in the fragment
-        recyclerView_food = (RecyclerView) getView().findViewById(R.id.recyclerView_food_id);
+        /*recyclerView_food = (RecyclerView) getView().findViewById(R.id.recyclerView_food_id);
         LinearLayoutManager rv_layoutManager2 = new LinearLayoutManager(getView().getContext(), LinearLayoutManager.HORIZONTAL,false);
         recyclerView_food.setLayoutManager(rv_layoutManager2);
-        recyclerView_food.setAdapter(new Popular_food_adapter(cards));
+        recyclerView_food.setAdapter(new Popular_food_adapter(cards));*/
 
         //Get the recycelrView of popular food and render it in the fragment
+        search_bar = (EditText)getView().findViewById(R.id.search_editText);
+        set_Search_bar();
         recyclerView_food = (RecyclerView) getView().findViewById(R.id.recyclerView_food_id2);
         LinearLayoutManager rv_layoutManager3 = new LinearLayoutManager(getView().getContext(), LinearLayoutManager.VERTICAL,false);
         recyclerView_food.setLayoutManager(rv_layoutManager3);
-        recyclerView_food.setAdapter(new Popular_food_adapter2(cards));
+        recyclerView_food.setAdapter(vertical_cards);
     }
 }
