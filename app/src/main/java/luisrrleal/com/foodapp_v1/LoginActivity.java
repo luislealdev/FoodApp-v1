2e1a5bbb9686df1a8584e8482f3ee326ad630f58
Alfredo <21030761@itcelaya.edu.mx>
2023-07-09 23:52:01 -0600

    Register user with firebase implemented

--- app/src/main/java/luisrrleal/com/foodapp_v1/LoginActivity.java
+++ app/src/main/java/luisrrleal/com/foodapp_v1/LoginActivity.java
@@ -1,5 +1,6 @@
 package luisrrleal.com.foodapp_v1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,18 +9,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText password, email;
     TextView create_account;
     Button login;
+    FirebaseAuth userAuth ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        password = (EditText) findViewById(R.id.password);
-        email = (EditText) findViewById(R.id.email);
+        userAuth = FirebaseAuth.getInstance();
+        password = (EditText) findViewById(R.id.passwordLogin);
+        email = (EditText) findViewById(R.id.emailLogin);
         login = (Button) findViewById(R.id.login);
         create_account = (TextView) findViewById(R.id.create_account);
         create_account.setOnClickListener(new View.OnClickListener() {
@@ -27,13 +36,29 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(), RegisterActivity.class);
                 startActivity(i);
+                finish();
             }
         });
         //loadFragment(Register_Fragment.newInstance());
     }
 
-    public void createUser(){
+    public void login(View v){
+        String emailText = email.getText().toString();
+        String passwordText = password.getText().toString();
 
+        userAuth.signInWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Login exitoso.", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(v.getContext(), MainActivity.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
     /*private void loadFragment(Fragment new_fragment){
         FragmentManager fm = getSupportFragmentManager();
--- app/src/main/java/luisrrleal/com/foodapp_v1/RegisterActivity.java
+++ app/src/main/java/luisrrleal/com/foodapp_v1/RegisterActivity.java
@@ -1,14 +1,64 @@
 package luisrrleal.com.foodapp_v1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText name, phone, email, password, confirmPassword;
+    ImageView getBack_button;
+    FirebaseAuth userAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        userAuth = FirebaseAuth.getInstance();
+        name = (EditText) findViewById(R.id.name);
+        phone = (EditText) findViewById(R.id.phone);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password1);
+        confirmPassword = (EditText) findViewById(R.id.password2);
+        getBack_button = (ImageView) findViewById(R.id.getBack_image);
+        backToLogin();
+    }
+
+    public void backToLogin(){
+        getBack_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+    public void registerUser(View v){
+        String emailText = email.getText().toString();
+        String passwordText = password.getText().toString();
+
+        userAuth.createUserWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Cuenta registrada con éxito.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
