d77117be1e2f288b3e418983514116a9024bf3ef
Alfredo <21030761@itcelaya.edu.mx>
2023-07-10 13:59:31 -0600

    Trying to use firestore to save users data

--- app/src/main/java/luisrrleal/com/foodapp_v1/RegisterActivity.java
+++ app/src/main/java/luisrrleal/com/foodapp_v1/RegisterActivity.java
@@ -5,22 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText name, phone, email, password, confirmPassword;
     ImageView getBack_button;
     FirebaseAuth userAuth;
+    DocumentReference document  = FirebaseFirestore.getInstance().document("/users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,10 +53,42 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
     public void registerUser(View v){
-        String emailText = email.getText().toString();
-        String passwordText = password.getText().toString();
+        String userName = name.getText().toString();
+        String userPhone = phone.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+        String userConfirmPassword = confirmPassword.getText().toString();
 
-        userAuth.createUserWithEmailAndPassword(emailText, passwordText)
+        if(userName.isEmpty() || userEmail.isEmpty() || userPassword.isEmpty() || userConfirmPassword.isEmpty()){
+            Toast.makeText(this, "Favor de llenar todos los campos", Toast.LENGTH_SHORT).show();
+        }else{
+            if(userPassword.equals(userConfirmPassword)){
+                Map<String, Object> user = new HashMap<>();
+                user.put("name", userName);
+                user.put("phone", userPhone);
+                user.put("email", userEmail);
+                user.put("password", userPassword);
+
+                document.set(user).
+                        addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(RegisterActivity.this, "Usuario reistrado con éxito", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(RegisterActivity.this, "Error al registrar el usuario", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }else{
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        userAuth.createUserWithEmailAndPassword(userEmail, userPassword)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
