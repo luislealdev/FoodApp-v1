f90a2fa730e4cc83d156d37835fd49d5bb7aec6b
Alfredo <21030761@itcelaya.edu.mx>
2023-07-09 22:30:39 -0600

    Replace register_fragment for activity

--- app/src/main/java/luisrrleal/com/foodapp_v1/LoginActivity.java
+++ app/src/main/java/luisrrleal/com/foodapp_v1/LoginActivity.java
@@ -1,23 +1,40 @@
 package luisrrleal.com.foodapp_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import luisrrleal.com.foodapp_v1.fragments.Register_Fragment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText password, email;
+    TextView create_account;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        password = (EditText) findViewById(R.id.password);
+        email = (EditText) findViewById(R.id.email);
+        login = (Button) findViewById(R.id.login);
+        create_account = (TextView) findViewById(R.id.create_account);
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), RegisterActivity.class);
+                startActivity(i);
+            }
+        });
         //loadFragment(Register_Fragment.newInstance());
     }
 
+    public void createUser(){
+
+    }
     /*private void loadFragment(Fragment new_fragment){
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
